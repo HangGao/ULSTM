4fd176956b19945759e3786a87886b57725e6b0b
HangGao <hanggao1@umbc.edu>
2018-04-10 20:13:22 -0400

    update classification

--- awd-lstm-lm/getdata_classification.sh
+++ awd-lstm-lm/getdata_classification.sh
@@ -1,10 +1,19 @@
-echo "- Downloading Glove"
-mkdir glove
-cd glove
-wget --continue http://nlp.stanford.edu/data/glove.840B.300d.zip
-unzip -o glove.840B.300d.zip
-rm *.zip
-cd ..
+cd data
 
-echo "- Downloading Yelp review data at https://www.yelp.com/dataset"
+# echo "- Downloading Glove"
+# mkdir glove
+# cd glove
+# wget --continue http://nlp.stanford.edu/data/glove.840B.300d.zip
+# unzip -o glove.840B.300d.zip
+# rm *.zip
+# cd ..
+
+echo "- Downloading stanford Sentiment Treebank"
+mkdir sts
+cd sts
+wget --continue http://nlp.stanford.edu/~socherr/stanfordSentimentTreebank.zip
+unzip -o stanfordSentimentTreebank.zip
+mv stanfordSentimentTreebank/* .
+rm -rf stanfordSentimentTreebank
+rm *.zip
 cd ..
--- awd-lstm-lm/review.py
+++ awd-lstm-lm/review.py
@@ -15,41 +15,37 @@ from utils import batchify, get_batch, repackage_hidden
 import os
 
 parser = argparse.ArgumentParser(description='PyTorch PennTreeBank RNN/LSTM Language Model')
-parser.add_argument('--data', type=str, default='data/yelp/',
+parser.add_argument('--data', type=str, default='data/sts/',
                     help='location of the data corpus')
 parser.add_argument('--model', type=str, default='LSTM',
                     help='type of recurrent net (LSTM, FPLSTM, QRNN, GRU)')
 parser.add_argument('--emsize', type=int, default=300,
                     help='size of word embeddings')
-parser.add_argument('--nhid', type=int, default=1150,
+parser.add_argument('--nhid', type=int, default=150,
                     help='number of hidden units per layer')
-parser.add_argument('--rhid', type=int, default=100,
-                    help='size of classifier hidden layer')
 parser.add_argument('--num_classes', type=int, default=5,
                     help='number of classed')
-parser.add_argument('--nlayers', type=int, default=2,
+parser.add_argument('--nlayers', type=int, default=1,
                     help='number of layers')
-parser.add_argument('--lr', type=float, default=30,
+parser.add_argument('--lr', type=float, default=0.05,
                     help='initial learning rate')
 parser.add_argument('--clip', type=float, default=0.25,
                     help='gradient clipping')
 parser.add_argument('--epochs', type=int, default=50,
                     help='upper epoch limit')
-parser.add_argument('--batch_size', type=int, default=50, metavar='N',
+parser.add_argument('--batch_size', type=int, default=25, metavar='N',
                     help='batch size')
-parser.add_argument('--bptt', type=int, default=70,
-                    help='sequence length')
-parser.add_argument('--dropout', type=float, default=0.4,
+parser.add_argument('--dropout', type=float, default=0.,
                     help='dropout applied to layers (0 = no dropout)')
-parser.add_argument('--dropouth', type=float, default=0.3,
+parser.add_argument('--dropouth', type=float, default=0.,
                     help='dropout for rnn layers (0 = no dropout)')
-parser.add_argument('--dropouti', type=float, default=0.65,
+parser.add_argument('--dropouti', type=float, default=0.2,
                     help='dropout for input embedding layers (0 = no dropout)')
-parser.add_argument('--dropoute', type=float, default=0.1,
+parser.add_argument('--dropoute', type=float, default=0.,
                     help='dropout to remove words from embedding layer (0 = no dropout)')
-parser.add_argument('--wdrop', type=float, default=0.5,
+parser.add_argument('--wdrop', type=float, default=0.25,
                     help='amount of weight dropout to apply to the RNN hidden to hidden matrix')
-parser.add_argument('--seed', type=int, default=1111,
+parser.add_argument('--seed', type=int, default=141,
                     help='random seed')
 parser.add_argument('--nonmono', type=int, default=5,
                     help='random seed')
@@ -60,15 +56,15 @@ parser.add_argument('--log-interval', type=int, default=20, metavar='N',
 randomhash = ''.join(str(time.time()).split('.'))
 parser.add_argument('--save', type=str,  default=randomhash+'.pt',
                     help='path to save the final model')
-parser.add_argument('--alpha', type=float, default=2,
+parser.add_argument('--alpha', type=float, default=0,
                     help='alpha L2 regularization on RNN activation (alpha = 0 means no regularization)')
-parser.add_argument('--beta', type=float, default=1,
+parser.add_argument('--beta', type=float, default=0,
                     help='beta slowness regularization applied on RNN activiation (beta = 0 means no regularization)')
-parser.add_argument('--wdecay', type=float, default=1.2e-6,
+parser.add_argument('--wdecay', type=float, default=0.0001,
                     help='weight decay applied to all weights')
 parser.add_argument('--resume', type=str,  default='',
                     help='path of model to resume')
-parser.add_argument('--optimizer', type=str,  default='sgd',
+parser.add_argument('--optimizer', type=str,  default='adagrad',
                     help='optimizer to use (sgd, adam)')
 parser.add_argument('--when', nargs="+", type=int, default=[-1],
                     help='When (which epochs) to divide the learning rate by 10 - accepts multiple')
@@ -89,6 +85,8 @@ if torch.cuda.is_available():
 ###############################################################################
 # Load data
 ###############################################################################
+import re
+
 class Dictionary(object):
     def __init__(self):
         self.word2idx = {}
@@ -114,71 +112,109 @@ class Corpus(object):
         self.PAD_TOKEN = '<PAD>'
         self.dictionary.add_word(self.PAD_TOKEN)
 
-        self.train = self.read_data(os.path.join(path, 'train.txt'))
-        self.valid = self.read_data(os.path.join(path, 'valid.txt'))
-        self.test = self.read_data(os.path.join(path, 'test.txt'))
+        self.train, self.valid, self.test = self.read_data(path)
 
     def read_data(self, path):
-        max_sent_len = 0
-        num_records = 0
-        with open(path, 'r') as f:
+        phrase2id = dict()
+        with open(os.path.join(path, 'dictionary.txt'), 'r') as f:
             for line in f:
-                num_records += 1
-                label, sent = line.strip().split('\t')
-                words = sent.split()
-                max_sent_len = max(max_sent_len, len(words))
+                line = re.sub(r'[^\x00-\x7F]',' ', line)
+                phrase, id = line.strip().split('|')
+                phrase2id[phrase] = id
+
+        id2sentiment = dict()
+        with open(os.path.join(path, 'sentiment_labels.txt'), 'r') as f:
+            f.readline()
+            for line in f:
+                line = re.sub(r'[^\x00-\x7F]',' ', line)
+                id, sentiment = line.strip().split('|')
+                id2sentiment[id] = float(sentiment)
+
+        sidx2text = dict()
+        with open(os.path.join(path, 'datasetSentences.txt'), 'r') as f:
+            f.readline()
+            for line in f:
+                line = re.sub(r'[^\x00-\x7F]',' ', line)
+                idx, sent = line.strip().split('\t')
+                sidx2text[idx] = sent
+
+        train = []
+        valid = []
+        test = []
+        max_sent_len = 0
+        with open(os.path.join(path, 'datasetSplit.txt'), 'r') as f:
+            f.readline()
+            for line in f:
+                line = re.sub(r'[^\x00-\x7F]',' ', line)
+                idx, split = line.strip().split(',')
+                text = sidx2text[idx]
+                if text not in phrase2id: continue
+                sentiment = id2sentiment[phrase2id[text]]
+                if sentiment <= 0.2:
+                    sentiment = 0
+                elif sentiment <= 0.4:
+                    sentiment = 1
+                elif sentiment <= 0.6:
+                    sentiment = 2
+                elif sentiment <= 0.8:
+                    sentiment = 3
+                else:
+                    sentiment = 4
+
+                words = text.split()
                 for word in words:
                     self.dictionary.add_word(word)
-        return num_records, max_sent_len, path
+                max_sent_len = max(max_sent_len, len(words))
 
-def batchify(dataset, dictionary, batch_size, cuda=True, max_cache_size=5000):
-    num_records, max_sent_len, path = dataset
-    n_batches = int(math.ceil(num_records / batch_size))
-    yield n_batches
+                if int(split) == 1:
+                    train.append((sentiment, text))
+                elif int(split) == 2:
+                    test.append((sentiment, text))
+                elif int(split) == 3:
+                    valid.append((sentiment, text))
+                else:
+                    raise Exception('wrong data file')
 
-    def next_buffer(fp):
-        records = []
-        batch_sent_len = 0
-        for i in range(max_cache_size):
-            line = fp.readline()
-            if line == '': break
-            label, sent = line.strip().split('\t')
-            records.append((label, sent))
-            batch_sent_len = max(batch_sent_len, len(sent.split()))
-        np.random.shuffle(records)
+        np.random.shuffle(train)
 
-        tlabel = torch.LongTensor(len(records))
-        ttext = torch.LongTensor(len(records), batch_sent_len).fill_(0)
-        for i in range(len(records)):
-            label, words = int(records[i][0]), records[i][1].split()
-            words = [dictionary.word2idx[word] for word in words]
-            tlabel[i] = label - 1
-            for j in range(len(words)):
-                ttext[i, j] = words[j]
-        ttext = ttext.t()
+        def to_torch_tensor(dataset):
+            targets = torch.LongTensor(len(dataset)).fill_(0)
+            texts = torch.LongTensor(len(dataset), max_sent_len).fill_(0)
 
-        if cuda:
-            tlabel = tlabel.cuda()
-            ttext = ttext.cuda()
-        return tlabel, ttext
+            for i in range(len(dataset)):
+                sentiment, text = dataset[i]
+                targets[i] = sentiment
+                words = text.split()
+                for j in range(len(words)):
+                    texts[i, j] = self.dictionary.word2idx[words[j]]
+            return texts, targets
 
-    while True:
-        fp = open(path, 'r')
+        train = to_torch_tensor(train)
+        valid = to_torch_tensor(valid)
+        test = to_torch_tensor(test)
 
-        lbuf, tbuf = next_buffer(fp)
-        bidx = 0
+        return train, valid, test
 
-        for batch in range(n_batches):
-            if bidx >= lbuf.size(0):
-                lbuf, tbuf = next_buffer(fp)
-                bidx = 0
+def batchify(dataset, batch_size, args):
+    data, target = dataset
 
-            bedx = min(lbuf.size(0), bidx+batch_size)
-            lbatch = lbuf[bidx:bedx]
-            tbatch = tbuf[:, bidx:bedx]
+    perm = torch.randperm(target.size(0))
+    data = data[perm]
+    target = target[perm]
 
-            bidx += batch_size
-            yield lbatch, tbatch
+    if args.cuda:
+        data = data.cuda()
+        target = target.cuda()
+
+    n_batches = int(math.ceil(target.size(0) / batch_size))
+    return (data, target), n_batches
+
+def get_batch(dataset, batch, batch_size):
+    data, target = dataset
+    sidx = batch * batch_size
+    eidx = min(target.size(0), (batch + 1) * batch_size)
+
+    return data[sidx:eidx].t(), target[sidx:eidx]
 
 ###############################################################################
 # Read Corpus
@@ -204,7 +240,7 @@ else:
     corpus = Corpus(args.data)
 
     print('Producing embeddings...')
-    if args.do_not_pretrain:
+    if not args.do_not_pretrain:
         embs = torch.Tensor(len(corpus.dictionary), args.emsize).uniform_(-0.05, 0.05)
     else:
         assert args.emsize == 300
@@ -227,11 +263,10 @@ else:
 # Build the model
 ###############################################################################
 class Network(nn.Module):
-    def __init__(self, rnn_type, ntoken, ninp, nhid, rhid, nlayers, num_classes, dropout=0.5, dropouth=0.5, dropouti=0.5, dropoute=0.1, wdrop=0):
+    def __init__(self, rnn_type, ntoken, ninp, nhid, nlayers, num_classes, dropout=0.5, dropouth=0.5, dropouti=0.5, dropoute=0.1, wdrop=0):
         super(Network, self).__init__()
         self.rnn = model.RNNModel(rnn_type, ntoken, ninp, nhid, nlayers, dropout, dropouth, dropouti, dropoute, wdrop, tie_weights=False, use_decoder=False)
-        self.layer_1 = nn.Linear(nhid, rhid)
-        self.layer_2 = nn.Linear(rhid, num_classes)
+        self.linear= nn.Linear(nhid, num_classes)
 
     def forward(self, sents, hids):
         """
@@ -242,21 +277,20 @@ class Network(nn.Module):
 
         seq_len = hids.size(0)
         batch_len = hids.size(1)
+
         hids = hids.view(-1, hids.size(2))
-
         batch_idx = Variable(torch.arange(batch_len).type(type(sents.data)))
-        seq_idx = batch_idx * seq_len + (torch.sum(torch.gt(sents, 0).type(type(sents.data)), 0) - 1)
+        seq_idx = torch.sum(torch.gt(sents, 0).type(type(sents.data)), 0) - 1
+        idx = seq_idx * batch_len + batch_idx
+        hids = torch.index_select(hids, 0, idx)
 
-        hids = torch.index_select(hids, 0, seq_idx)
-
-        output = F.relu(self.layer_1(hids))
-        output = F.log_softmax(self.layer_2(output), 1)
+        output = F.log_softmax(self.linear(hids), 1)
 
         return output, rnn_hs, dropped_rnn_hs
 
 criterion = nn.NLLLoss()
 ntokens = len(corpus.dictionary)
-model = Network(args.model, ntokens, args.emsize, args.nhid, args.rhid, args.nlayers, args.num_classes, args.dropout, \
+model = Network(args.model, ntokens, args.emsize, args.nhid, args.nlayers, args.num_classes, args.dropout, \
                 args.dropouth, args.dropouti, args.dropoute, args.wdrop)
 model.rnn.encoder.weight.data.copy_(embs)
 # model.rnn.encoder.weight.requires_grad = False
@@ -273,39 +307,37 @@ total_params = sum(x.size()[0] * x.size()[1] if len(x.size()) > 1 else x.size()[
 print('Args:', args)
 print('Model total parameters:', total_params)
 
-train_data = batchify(corpus.train, corpus.dictionary, args.batch_size, args.cuda)
-val_data = batchify(corpus.valid,  corpus.dictionary, args.batch_size, args.cuda)
-test_data = batchify(corpus.test,  corpus.dictionary, args.batch_size, args.cuda)
+val_data, val_n_batches = batchify(corpus.valid, args.batch_size, args)
+test_data, test_n_batches = batchify(corpus.test, args.batch_size, args)
 
 ##############################################################################
 # Training code
 ##############################################################################
-def evaluate(data_source, batch_size=50):
+def evaluate(data_source, nbatch, batch_size=50):
     # Turn on evaluation mode which disables dropout.
     model.eval()
     if args.model == 'QRNN': model.reset()
     total_loss = 0
     ntokens = len(corpus.dictionary)
 
-    hidden = model.rnn.init_hidden(args.batch_size)
-    nbatch = next(data_source)
-
     correct, total = 0., 0.
     for batch in range(nbatch):
-        lbatch, tbatch = next(train_data)
+        tbatch, lbatch = get_batch(data_source, batch, args.batch_size)
         lbatch, tbatch = Variable(lbatch), Variable(tbatch)
 
         # output (batch_size, num_classes)
-        hidden = repackage_hidden(hidden)
+        hidden = model.rnn.init_hidden(lbatch.size(0))
         output, rnn_hs, dropped_rnn_hs = model(tbatch, hidden)
         raw_loss = criterion(output, lbatch)
+        total_loss += raw_loss.data
 
-        prediction = torch.argmax(torch.exp(output), dim=1)
-        crt = torch.eq(prediction, lbatch).type(torch.LongTensor)
-        correct += torch.sum(crt)
-        total += prediction.size(0)
+        output = torch.exp(output).data.numpy()
+        prediction = np.argmax(output, 1)
+        crt = np.asarray(np.equal(prediction, lbatch.data.numpy()), 'int32')
+        correct += np.sum(crt)
+        total += prediction.shape[0]
 
-    return total_loss[0], correct / total
+    return total_loss[0]/nbatch, correct / total
 
 
 def train():
@@ -318,17 +350,14 @@ def train():
     start_time = time.time()
     ntokens = len(corpus.dictionary)
 
-    hidden = model.rnn.init_hidden(args.batch_size)
-
-    nbatch = next(train_data)
+    train_data, nbatch = batchify(corpus.train, args.batch_size, args)
     for batch in range(nbatch):
         model.train()
 
-        lbatch, tbatch = next(train_data)
+        tbatch, lbatch = get_batch(train_data, batch, args.batch_size)
         lbatch, tbatch = Variable(lbatch), Variable(tbatch)
-        # Starting each batch, we detach the hidden state from how it was previously produced.
-        # If we didn't, the model would try backpropagating all the way to start of the dataset.
-        hidden = repackage_hidden(hidden)
+        hidden = model.rnn.init_hidden(lbatch.size(0))
+
         optimizer.zero_grad()
 
         # output (batch_size, num_classes)
@@ -359,7 +388,7 @@ def train():
 # Loop over epochs.
 lr = args.lr
 best_val_loss = []
-stored_acc = 10000000
+stored_loss = 10000000
 
 # At any point you can hit Ctrl + C to break out of training early.
 try:
@@ -381,7 +410,7 @@ try:
                 tmp[prm] = prm.data.clone()
                 prm.data = optimizer.state[prm]['ax'].clone()
 
-            val_loss2, val_acc2 = evaluate(val_data)
+            val_loss2, val_acc2 = evaluate(val_data, val_n_batches)
             print('-' * 89)
             print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | valid acc {:5.2f}'.format(
               epoch, (time.time() - epoch_start_time), val_loss2, val_acc2))
@@ -396,7 +425,7 @@ try:
                 prm.data = tmp[prm].clone()
 
         else:
-            val_loss, val_acc = evaluate(val_data, eval_batch_size)
+            val_loss, val_acc = evaluate(val_data, val_n_batches)
             print('-' * 89)
             print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | val acc {:5.2f}'.format(
               epoch, (time.time() - epoch_start_time), val_loss, val_acc))
@@ -427,14 +456,14 @@ except KeyboardInterrupt:
 model_load(args.save)
 
 # Run on valid data.
-val_loss, val_acc = evaluate(val_data)
+val_loss, val_acc = evaluate(val_data, val_n_batches)
 print('=' * 89)
 print('| End of training | valid loss {:5.2f} | valid acc {:5.2f}'.format(
     val_loss, val_acc))
 print('=' * 89)
 
 # Run on test data.
-test_loss, test_acc = evaluate(test_data)
+test_loss, test_acc = evaluate(test_data, test_n_batches)
 print('=' * 89)
 print('| End of training | test loss {:5.2f} | test acc {:5.2f}'.format(
     test_loss, test_acc))
