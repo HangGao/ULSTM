5575a5850c44c55d3191b609aacfaf943e6c2a6c
HangGao <hanggao1@umbc.edu>
2018-07-02 17:10:18 -0400

    bug fix

--- sentiment/run.sh
+++ sentiment/run.sh
@@ -7,16 +7,16 @@
 # CUDA_VISIBLE_DEVICES=2 python review.py --model PLSTM --nlayers 1 --nhid 250 --save save/PLSTM_1_250.pt > save/PLSTM_1_250.out &
 # CUDA_VISIBLE_DEVICES=2 python review.py --model PLSTM --nlayers 1 --nhid 300 --save save/PLSTM_1_300.pt > save/PLSTM_1_300.out &
 
-CUDA_VISIBLE_DEVICES=0 python review.py --model PLSTM --nlayers 2 --nhid 50 --save save/PLSTM_2_50.pt > save/PLSTM_2_50.out &
-CUDA_VISIBLE_DEVICES=0 python review.py --model PLSTM --nlayers 2 --nhid 100 --save save/PLSTM_2_100.pt > save/PLSTM_2_100.out &
-CUDA_VISIBLE_DEVICES=1 python review.py --model PLSTM --nlayers 2 --nhid 150 --save save/PLSTM_2_150.pt > save/PLSTM_2_150.out &
-CUDA_VISIBLE_DEVICES=1 python review.py --model PLSTM --nlayers 2 --nhid 200 --save save/PLSTM_2_200.pt > save/PLSTM_2_200.out &
-CUDA_VISIBLE_DEVICES=2 python review.py --model PLSTM --nlayers 2 --nhid 250 --save save/PLSTM_2_250.pt > save/PLSTM_2_250.out &
-CUDA_VISIBLE_DEVICES=2 python review.py --model PLSTM --nlayers 2 --nhid 300 --save save/PLSTM_2_300.pt > save/PLSTM_2_300.out &
+# CUDA_VISIBLE_DEVICES=0 python review.py --model PLSTM --nlayers 2 --nhid 50 --save save/PLSTM_2_50.pt > save/PLSTM_2_50.out &
+# CUDA_VISIBLE_DEVICES=0 python review.py --model PLSTM --nlayers 2 --nhid 100 --save save/PLSTM_2_100.pt > save/PLSTM_2_100.out &
+# CUDA_VISIBLE_DEVICES=1 python review.py --model PLSTM --nlayers 2 --nhid 150 --save save/PLSTM_2_150.pt > save/PLSTM_2_150.out &
+# CUDA_VISIBLE_DEVICES=1 python review.py --model PLSTM --nlayers 2 --nhid 200 --save save/PLSTM_2_200.pt > save/PLSTM_2_200.out &
+# CUDA_VISIBLE_DEVICES=2 python review.py --model PLSTM --nlayers 2 --nhid 250 --save save/PLSTM_2_250.pt > save/PLSTM_2_250.out &
+# CUDA_VISIBLE_DEVICES=2 python review.py --model PLSTM --nlayers 2 --nhid 300 --save save/PLSTM_2_300.pt > save/PLSTM_2_300.out &
 
-# CUDA_VISIBLE_DEVICES=0 python review.py --model PLSTM --nlayers 3 --nhid 50 --save save/PLSTM_3_50.pt > save/PLSTM_3_50.out &
-# CUDA_VISIBLE_DEVICES=0 python review.py --model PLSTM --nlayers 3 --nhid 100 --save save/PLSTM_3_100.pt > save/PLSTM_3_100.out &
-# CUDA_VISIBLE_DEVICES=1 python review.py --model PLSTM --nlayers 3 --nhid 150 --save save/PLSTM_3_150.pt > save/PLSTM_3_150.out &
-# CUDA_VISIBLE_DEVICES=1 python review.py --model PLSTM --nlayers 3 --nhid 200 --save save/PLSTM_3_200.pt > save/PLSTM_3_200.out &
-# CUDA_VISIBLE_DEVICES=2 python review.py --model PLSTM --nlayers 3 --nhid 250 --save save/PLSTM_3_250.pt > save/PLSTM_3_250.out &
-# CUDA_VISIBLE_DEVICES=2 python review.py --model PLSTM --nlayers 3 --nhid 300 --save save/PLSTM_3_300.pt > save/PLSTM_3_300.out &
+CUDA_VISIBLE_DEVICES=0 python review.py --model PLSTM --nlayers 3 --nhid 50 --save save/PLSTM_3_50.pt > save/PLSTM_3_50.out &
+CUDA_VISIBLE_DEVICES=0 python review.py --model PLSTM --nlayers 3 --nhid 100 --save save/PLSTM_3_100.pt > save/PLSTM_3_100.out &
+CUDA_VISIBLE_DEVICES=1 python review.py --model PLSTM --nlayers 3 --nhid 150 --save save/PLSTM_3_150.pt > save/PLSTM_3_150.out &
+CUDA_VISIBLE_DEVICES=1 python review.py --model PLSTM --nlayers 3 --nhid 200 --save save/PLSTM_3_200.pt > save/PLSTM_3_200.out &
+CUDA_VISIBLE_DEVICES=2 python review.py --model PLSTM --nlayers 3 --nhid 250 --save save/PLSTM_3_250.pt > save/PLSTM_3_250.out &
+CUDA_VISIBLE_DEVICES=2 python review.py --model PLSTM --nlayers 3 --nhid 300 --save save/PLSTM_3_300.pt > save/PLSTM_3_300.out &
